3368be761c5ec4381a984347c027f326cbb40013
Aditya <aditya.chandel@aggiemail.usu.edu>
2018-11-14 22:55:33 -0800

    V2

--- src/main/java/com/spring/pipeline/demo/DemoApplication.java
+++ src/main/java/com/spring/pipeline/demo/DemoApplication.java
@@ -1,10 +1,8 @@
 package com.spring.pipeline.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +20,7 @@ public class DemoApplication {
 
     @GetMapping("/active-profile")
     public String getEnvironment() {
-        return activeProfile;
+        return activeProfile + "V2";
     }
 
 }
